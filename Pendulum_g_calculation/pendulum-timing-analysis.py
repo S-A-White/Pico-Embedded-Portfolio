e2a1d06897a0dc62e1e50f6feff0b2388a27f67f
Sean W <sean.white24@proton.me>
2026-08-17 09:56:48 +0100

    Finishing off setup
    Completed code of pendulum analysis, ready for commentation

--- Pendulum_g_calculation/pendulum-timing-analysis.py
+++ Pendulum_g_calculation/pendulum-timing-analysis.py
@@ -18,9 +18,30 @@ gate.irq(trigger = Pin.IRQ_FALLING, handler = gate_handler)
 print("Swing the pendulum throught the gate to start data recording")
 
 try:
-    whil True:
-    time.sleep_ms(50)
+    while True:
+        time.sleep_ms(50)
 except KeyboardInterrupt:
     print("Stopped.")
 
 #analysing results (done in same code file as collection for pico efficiency)
+
+print(f"\nTotal beam breaks: {len(break_ts)}")
+
+if len(break_ts) >= 3:
+    periods = []
+    for i in range(2, len(break_ts)):
+        period = time.ticks_diff(break_ts[i], break_ts[i-2]) / 1000
+        periods.append(period)
+
+        avg_T = sum(periods) / len(periods)
+        print(f"Individual Periods (s): {[round(p,3) for p in periods]}")
+        print(f"Average Period (s): {round(avg_T, 3)}")
+        print(f"Average Periods (T) = {avg_T:.4g} s")
+
+else:
+    print("Not eenough beam breaks to find a period, try swinging the pendulum again for more oscillations")
+
+L = 0.78486 #Length of pendulum in meters
+g = 4*math.pi**2*(L/(avg_T**2)) #Calculating g using the formula g = 4π²L/T² 
+g_diff = g - 9.81582 #measured value compared to accuraely knowl local value of g (obtained from the national geographic survey
+print(f"The value of g found by this pendulum setup was {g:.4f}, which is accurate to ±{abs(g_diff):.4f}")
